71ce0eb6bc9f20d2e5e099a1df9e981c1e3939d6
MaYaSeVeN <mayaseven@hotmail.com>
2012-01-10 04:32:36 +0700

    Complete Stop and Wait algorithm.

--- networkProject.py
+++ networkProject.py
@@ -1,36 +1,55 @@
 #!/usr/bin/env python
 #-*- coding:UTF-8 -*-
 import serial, os, hashlib, binascii, base64
+
 def main():
     os.system(['clear', 'cls'][os.name == 'nt'])
-    print   "###################################################"
-    print    " __  __    __     __    _____   __      __  _   _"
-    print    "|  \/  |   \ \   / /   / ____|  \ \    / / | \ | |"
-    print    "| \  / | __ \ \_/ /_ _| (___   __\ \  / /__|  \| |"
-    print    "| |\/| |/ _\ \   / _\ |\___ \ / _ \ \/ / _ \ . \ |"
-    print    "| |  | | (_| || | (_| |____) |  __/\  /  __/ |\  |"
-    print    "|_|  |_|\__,_||_|\__,_|_____/ \___| \/ \___|_| \_|"
-    print   " "
-    print   "###################################################"
-    print   ""
-    print   "http://mayaseven.blogspot.com"
-    print   ""
-    
-    input_mode = raw_input("Computer Network Project 2011\n\nSelect Mode :\n    1.)Send\n    2.)Receive\n    3.)Show info send port\n    4.)Show info receive port\n\nInput number : ")
-    if (input_mode.isdigit()):
-        input_mode = int(input_mode)
-        if(input_mode == 1 or input_mode == 2 or input_mode == 3 or input_mode == 4):
-            if(input_mode == 1):
-                input_file = raw_input("Input file to send : ")
-                send(input_file, -1)
-            elif(input_mode == 2): 
-                receive(-1)  
-            elif(input_mode == 3):
-                print "\nShow info send port\n"
-                send(-1, 1)
-            elif(input_mode == 4):
-                print "\nShow info receive port\n"
-                receive(1)
+    print '''
+   _____                             _             
+  /  __ \                           | |            
+  | /  \/ ___  _ __ ___  _ __  _   _| |_  ___ _ __ 
+  | |    / _ \| '_ ` _ \| '_ \| | | | __|/ _ \ '__|
+  | \__/\ (_) | | | | | | |_) | |_| | |_|  __/ |   
+   \____/\___/|_| |_| |_| .__/ \__,_|\__|\___|_|   
+                        | |                        
+                        |_|                        
+
+           _   _      _                      _    
+          | \ | |    | |                    | |   
+          |  \| | ___| |___      _____  _ __| | __
+          | . ` |/ _ \ __\ \ /\ / / _ \| '__| |/ /
+          | |\  |  __/ |_ \ V  V / (_) | |  |   < 
+          \_| \_/\___|\__| \_/\_/ \___/|_|  |_|\_\\
+                                                  
+                                                  
+
+                 ______           _           _        _____  _____  __   __  
+                 | ___ \         (_)         | |      / __  \|  _  |/  | /  | 
+                 | |_/ /_ __ ___  _  ___  ___| |_     `' / /'| |/' |`| | `| | 
+                 |  __/| '__/ _ \| |/ _ \/ __| __|      / /  |  /| | | |  | | 
+                 | |   | | | (_) | |  __/ (__| |_     ./ /___\ |_/ /_| |__| |_
+                 \_|   |_|  \___/| |\___|\___|\__|    \_____/ \___/ \___/\___/
+                                _/ |                                          
+                               |__/                                           
+       
+'''
+    print   "MaYaSeVeN -- http://mayaseven.blogspot.com"
+    print   "Methuz -- http://joinstick.net"
+    inputMode = raw_input("\nSelect Mode :\n    1.)SendSW\n    2.)ReceiveSW\n    3.)SendSR\n    4.)ReceiveSR\n\nInput number : ")
+    print   "" 
+    if (inputMode.isdigit()):
+        inputMode = int(inputMode)
+        if(inputMode == 1 or inputMode == 2 or inputMode == 3 or inputMode == 4):
+            if(inputMode == 1):
+                inputFile = raw_input("Input filename to send : ")
+                sendSW(inputFile)
+            elif(inputMode == 2):
+                receiveSW()
+            elif(inputMode == 3):
+                inputFile = raw_input("Input filename to send : ")
+                sendSR(inputFile)
+            elif(inputMode == 4):
+                receiveSR()
         else:
             print "\nInput number 1 - 4 !!!!"
     else:
@@ -59,91 +78,90 @@ def packetMake(data, sizeFile):
             numberPacket += 1
     return packet, numberPacket
     
-def send(input_file, check):
+def sendSW(inputFile):
     ser = serial.Serial(
-    port='COM1',
-    baudrate=9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=2
+        port='COM3',
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=7
     )
-    if(check == 1):
-        print ser.name
-        print ser.isOpen
+    try:
+        f = open(inputFile, 'rb')
+        md5file = "MD5Sum of " + inputFile + " : " + hashlib.md5(f.read()).hexdigest()
+        f.flush
+        f = open(inputFile, 'rb')
+        data = f.read()
+        f.close()
+    except IOError:
+        print "Error: can\'t find file or read data"
     else:
-        try:
-            f = open(input_file, 'rb')
-            md5file = "MD5Sum of "+ input_file + " : " + hashlib.md5(f.read()).hexdigest()
-            f.flush
-            f = open(input_file, 'rb')
-            data = f.read()
-            f.close()
-        except IOError:
-            print "Error: can\'t find file or read data"
-        else:
-            sizeFile = os.stat(input_file).st_size
-            packet, numberPacket = packetMake(data, sizeFile)
-            print "Sending file size is %s" % sizeFile 
-            print "Total number of packet is %s" % numberPacket
-            
-            i = 0
-            handshanke = str(numberPacket) + " " + input_file + "\n"
-            ser.write(handshanke)
-            while i < (numberPacket):
-                ser.write(base64.b64encode(packet[i]) + "\n")
-                if(ser.read(3) != "ACK"):
-                    continue 
-                else:
-                    print "Sending packet number " + str(i)
-                    i += 1
-            print "\n[+] Sending Complete. Filename is %s" %input_file
-            print md5file
+        sizeFile = os.stat(inputFile).st_size
+        packet, numberPacket = packetMake(data, sizeFile)    
+        i = 0
+        handshanke = str(numberPacket) + " " + inputFile + "\n"
+        ser.write(handshanke)
+        while i < (numberPacket):
+            ser.write(base64.b64encode(packet[i]) + "\n")
+            print "Sending packet number " + str(i) 
+            if(int(ser.readline()) != i):
+                continue 
+            else:
+                print "ACK " + str(i) + " Received\n"
+                i += 1
+        print "\nTotal number of packet is %s" % numberPacket
+        print "Sending file size is %s" % sizeFile 
+        print md5file
+        print "\n[+] Sending Complete. Filename is %s" % inputFile
     ser.flush()       
     
-def receive(check):      
+def receiveSW():      
     ser = serial.Serial(
-            port='COM2',
-            baudrate=9600,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.EIGHTBITS,
-            timeout=4
-            )
-    
-    if(check == 1):
-        print ser.name
-        print ser.isOpen
-    else:
-        handshake = ser.readline()
-        numberPacket = int(handshake.split()[0])
-        k = 0
-        packetR = []
-        print numberPacket
-        while k < numberPacket:       
-            packetR.append(base64.b64decode(ser.readline()))
-            crcCheck = packetR[k][-8:]
-            crcPacket = binascii.crc32(packetR[k][:-8]) & 0xffffffff
-            print "Packet number %s's header is %s" % (k, packetR[k][:8])
+        port='COM4',
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=7
+    )
+    handshake = ser.readline()
+    numberPacket = int(handshake.split()[0])
+    k = 0
+    packetR = []
+    while k < numberPacket:       
+        packetR.append(base64.b64decode(ser.readline()))
+        crcCheck = packetR[k][-8:]
+        crcPacket = binascii.crc32(packetR[k][:-8]) & 0xffffffff
+        print "Packet number %s's header is %s" % (k, packetR[k][:8])
+        if crcCheck == "%08x" %crcPacket:
             print "Packet number %s CRC is checked " % k + "(%s,%08x)" % (crcCheck, crcPacket)
-            ser.write("ACK")
-            print "Sending ACK %s" % k + "\n"
-            k += 1
-        ser.close()
-        p = 0
-        data = ""
-        while p < len(packetR):
-            data += packetR[p][8:-8]
-            p += 1
-        receiveFile = 'Receive'+"_"+handshake.split()[1]
-        f = open(receiveFile, 'wb')
-        for l in data:
-            f.write(l)
-        f.flush()
-        f = open(receiveFile, 'rb')
-        print "[+] Receiving Complete. Filename is %s" %receiveFile
-        print "MD5Sum of "+ receiveFile + " : " + hashlib.md5(f.read()).hexdigest()
-        f.flush       
+            if int(packetR[k][2:6],16) == k:
+                ser.write("%d" %k + "\n")
+                print "Sending ACK %s" % k + "\n"
+            else:
+                continue
+        else:
+            continue
+        k += 1
+    ser.close()
+    p = 0
+    data = ""
+    while p < len(packetR):
+        data += packetR[p][8:-8]
+        p += 1
+    receiveFile = 'Receive' + "_" + handshake.split()[1]
+    f = open(receiveFile, 'wb')
+    for l in data:
+        f.write(l)
+    f.flush()
+    f = open(receiveFile, 'rb')
+    print "MD5Sum of " + receiveFile + " : " + hashlib.md5(f.read()).hexdigest()
+    print "\n[+] Receiving Complete. Filename is %s" % receiveFile
+    f.flush       
     os.system(receiveFile)
-    print "Completed"
+def sendSR(inputFile):
+    pass
+def receiveSR():
+    pass
 main()
